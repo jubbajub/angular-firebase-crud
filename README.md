d4d99c1c842180eeaa5c01b02c8f3fb4f12cdeab
jubbajub <sefer.derya@gmx.de>
2020-01-17 21:58:55 +0100

    STEP 6 -->DONE

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Step 2 - Creating a Firebase Project and a Firestore Database -->DONE
 Step 3 - Installing and Adding Firebase to your Angular 8 Project.-->DONE
 Step 4 - Create an Angular 8 Model -->DONE
 Step 5 - Creating an Angular 8 Service -->DONE
-Step 6 - Creating a Component for Making CRUD Operations
+Step 6 - Creating a Component for Making CRUD Operations -->DONE
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
+import { PolicyListComponent } from './policy-list/policy-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolicyListComponent
   ],
   imports: [
     BrowserModule,
